74c4c3e248abd9a5d7812df72e8638237fee4bd9
Bitan Mondal <bitanmondal011@gmail.com>
2020-11-11 21:04:01 +0530

    Initial commit

---
+++ src/main/java/io/weather/restclients/WeatherRestClient.java
@@ -0,0 +1,23 @@
+package io.weather.restclients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import io.weather.models.DailyTempBO;
+import io.weather.models.WeatherBO;
+
+@FeignClient(url="http://api.openweathermap.org/data/2.5/",name="weather-client")
+public interface WeatherRestClient {
+	@GetMapping("/weather")
+	public WeatherBO getWeatherOfParticularLocation(@RequestParam(value="appid") String appid,@RequestParam(value="q") String locationName);
+	
+	@GetMapping("/onecall")
+	public DailyTempBO getDailyTemperatureFromLatAndLong(@RequestParam(value="lat") String lat,@RequestParam(value="lon") String lon,
+			@RequestParam(value="appid") String appid,@RequestParam(value="exclude") String exclude);
+	
+	
+	@GetMapping("/weather")
+	public WeatherBO getWeatherOfParticularLocationForName(@RequestParam(value="appid") String appid,@RequestParam(value="lat") String lat,@RequestParam(value="lon") String lon);
+	
+}
